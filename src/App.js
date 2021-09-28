5e9ffd9eebc4e41997b64d3b60df63376993189a
patrickgsda <patrickarruda30@gmail.com>
2021-09-27 22:30:35 -0300

    OK.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import FruitsPage from "./pages/FruitsPage";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FruitsPage></FruitsPage>
+      </header>
+    </div>
+  );
+}
+
+export default App;
